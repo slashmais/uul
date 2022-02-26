c24af9375021afaf5ee95954e10c24b5030372d9
slashmais <slashmais@gmail.com>
2022-02-26 11:21:57 +0200

    Update uul.h

--- uul.h
+++ uul.h
@@ -4,13 +4,28 @@
 #include <string>
 #include <utilfuncs/utilfuncs.h>
 
+/*
+  uucase, ulcase
+    process & return:
+      converts and return the upper/lower case value of 'cp'
+*/
 unicodepoint uucase(unicodepoint cp);
 unicodepoint ulcase(unicodepoint cp);
 //use Utou8() to convert unicodepoint to utf8-char (in utilfuncs)
 
-//text that contain utf8 chars
+/*
+  uucase, ulcase
+    process & return:
+      converts (for every utf8 char in stext) and return the upper/lower cased 'stext'
+*/
 std::string uucase(const std::string &stext);
 std::string ulcase(const std::string &stext);
+
+/*
+  touucase, toulcase
+    process & return:
+      converts every utf8 char in stext in-place to upper/lower case
+*/
 inline void touucase(std::string &stext) { stext=uucase(stext); }
 inline void toulcase(std::string &stext) { stext=ulcase(stext); }
 
